7892989f88e5e180654c465ef94f1affd7efa4f7
Mauricio Etcheverry <mauricio@yoreparo.com>
2021-05-16 20:47:34 -0300

    Politica privacidad

---
+++ pages/politica-de-privacidad.vue
@@ -0,0 +1,143 @@
+<template>
+    <div>
+        <section class="page-title centred" style="background-image: url(/images/fondo-amarillo-escuelita-dev.jpg);">
+            <div class="container">
+                <div class="content-box">
+                    <h1>Política de Privacidad de Escuelita.dev</h1>
+                    <ul class="bread-crumb clearfix">
+                        <li>
+                            <NuxtLink to="/">Inicio</NuxtLink>                            
+                        </li>
+                        <li>Política de Privacidad</li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+        <section class="about-section style-two">
+        <div class="anim-icon">
+            <div class="icon icon-1 float-bob-x"></div>
+            <div class="icon icon-3"></div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 content-column">
+                    <div class="content-box">
+                        <div class="sec-title style-two">
+                            <h1>Política de Privacidad</h1>
+                        </div>
+                        <div class="bold-text">
+                            <p>Escuelita.dev le informa sobre su Política de Privacidad respecto del tratamiento y protección de los datos de carácter personal de los usuarios y clientes que puedan ser recabados por la navegación o contratación de servicios a través del sitio Web Escuelita.dev.</p>
+                            <p>En este sentido, el Titular garantiza el cumplimiento de la normativa vigente en materia de protección de datos personales, reflejada en la Ley Orgánica 3/2018, de 5 de diciembre, de Protección de Datos Personales y de Garantía de Derechos Digitales (LOPD GDD). Cumple también con el Reglamento (UE) 2016/679 del Parlamento Europeo y del Consejo de 27 de abril de 2016 relativo a la protección de las personas físicas (RGPD).</p>
+                            <p>El uso de sitio Web implica la aceptación de esta Política de Privacidad así como las condiciones incluidas en el Aviso Legal.</p>
+                            <h1>Identidad del responsable</h1>
+                            <ul>
+                                <li>Titular: Escuelita.dev</li>
+                                <li>Domicilio: Tiburcio Gómez 1330, Montevideo, Uruguay.</li>
+                                <li>Correo Electrónico: hola@escuelita.dev</li>
+                                <li>Sitio web: https://escuelita.dev</li>
+                            </ul>
+                            <h1>Principios aplicados en el tratamiento de datos</h1>
+                            <p>En el tratamiento de tus datos personales, el Titular aplicará los siguientes principios que se ajustan a las exigencias del nuevo reglamento europeo de protección de datos:</p>
+                            <ul>
+                                <li>Principio de licitud, lealtad y transparencia: El Titular siempre requerirá el consentimiento para el tratamiento de tus datos personales que puede ser para uno o varios fines específicos sobre los que te informará previamente con absoluta transparencia.</li>
+                                <li>Principio de minimización de datos: El Titular te solicitará solo los datos estrictamente necesarios para el fin o los fines que los solicita.</li>
+                                <li>Principio de limitación del plazo de conservación: Los datos se mantendrán durante el tiempo estrictamente necesario para el fin o los fines del tratamiento.</li>
+                                <li>El Titular te informará del plazo de conservación correspondiente según la finalidad. En el caso de suscripciones, el Titular revisará periódicamente las listas y eliminará aquellos registros inactivos durante un tiempo considerable.</li>
+                                <li>Principio de integridad y confidencialidad: Tus datos serán tratados de tal manera que su seguridad, confidencialidad e integridad esté garantizada. Debes saber que el Titular toma las precauciones necesarias para evitar el acceso no autorizado o uso indebido de los datos de sus usuarios por parte de terceros.</li>
+                            </ul>
+                            <h1>Obtención de datos personales</h1>
+                            <p>Para navegar por Escuelita.dev no es necesario que facilites ningún dato personal. Los casos en los que sí proporcionas tus datos personales son los siguientes:</p>
+                            <ul>
+                                <li>Al contactar a través de los formularios de contacto o enviar un correo electrónico.</li>
+                                <li>Al realizar un comentario en un artículo o página.</li>
+                                <li>Al inscribirte en un formulario de suscripción o un boletín.</li>
+                            </ul>
+                            <h1>Tus derechos</h1>
+                            <p>El Titular te informa que sobre tus datos personales tienes derecho a:</p>
+                            <ul>
+                                <li>Solicitar el acceso a los datos almacenados.</li>
+                                <li>Solicitar una rectificación o la cancelación.</li>
+                                <li>Solicitar la limitación de su tratamiento.</li>
+                                <li>Oponerte al tratamiento.</li>
+                                <li>Solicitar la portabilidad de tus datos.</li>
+                            </ul>
+                            <p>El ejercicio de estos derechos es personal y por tanto debe ser ejercido directamente por el interesado, solicitándolo directamente al Titular, lo que significa que cualquier cliente, suscriptor o colaborador que haya facilitado sus datos en algún momento puede dirigirse al Titular y pedir información sobre los datos que tiene almacenados y cómo los ha obtenido, solicitar la rectificación de los mismos, solicitar la portabilidad de sus datos personales, oponerse al tratamiento, limitar su uso o solicitar la cancelación de esos datos en los ficheros del Titular.</p>
+                            <p>Para ejercitar tus derechos de acceso, rectificación, cancelación, portabilidad y oposición tienes que enviar un correo electrónico a hola@escuelita.dev junto con la prueba válida en derecho como una fotocopia del D.N.I. o equivalente.</p>
+                            <p>Tienes derecho a la tutela judicial efectiva y a presentar una reclamación ante la autoridad de control, en este caso, la Unidad Reguladora y de Control de Datos Personales (Uruguay), si consideras que el tratamiento de datos personales que te conciernen infringe el Reglamento.</p>
+                            <h1>Finalidad del tratamiento de datos personales</h1>
+                            <p>Cuando te conectas al sitio Web para mandar un correo al Titular, te suscribes a su boletín o realizas una contratación, estás facilitando información de carácter personal de la que el responsable es el Titular. Esta información puede incluir datos de carácter personal como pueden ser tu dirección IP, nombre y apellidos, dirección física, dirección de correo electrónico, número de teléfono, y otra información. Al facilitar esta información, das tu consentimiento para que tu información sea recopilada, utilizada, gestionada y almacenada por superadmin.es , sólo como se describe en el Aviso Legal y en la presente Política de Privacidad.</p>
+                            <p>Los datos personales y la finalidad del tratamiento por parte del Titular es diferente según el sistema de captura de información:</p>
+                            <ul>
+                                <li>Formularios de contacto: El Titular solicita datos personales entre los que pueden estar: Nombre y apellidos, dirección de correo electrónico, número de teléfono y dirección de tu sitio Web con la finalidad de responder a tus consultas.
+Por ejemplo, el Titular utiliza esos datos para dar respuesta a tus mensajes, dudas, quejas, comentarios o inquietudes que puedas tener relativas a la información incluida en el sitio Web, los servicios que se prestan a través del sitio Web, el tratamiento de tus datos personales, cuestiones referentes a los textos legales incluidos en el sitio Web, así como cualquier otra consulta que puedas tener y que no esté sujeta a las condiciones del sitio Web o de la contratación.</li>
+                            </ul>
+
+                            <p>Existen otras finalidades por las que el Titular trata tus datos personales:</p>
+                            <ul>
+                                <li>Para garantizar el cumplimiento de las condiciones recogidas en el Aviso Legal y en la ley aplicable. Esto puede incluir el desarrollo de herramientas y algoritmos que ayuden a este sitio Web a garantizar la confidencialidad de los datos personales que recoge.</li>
+                                <li>Para apoyar y mejorar los servicios que ofrece este sitio Web.</li>
+                                <li>Para analizar la navegación. El Titular recoge otros datos no identificativos que se obtienen mediante el uso de cookies que se descargan en tu ordenador cuando navegas por el sitio Web cuyas caracterísiticas y finalidad están detalladas en la Política de Cookies .</li>
+                                <li>Para gestionar las redes sociales. el Titular tiene presencia en redes sociales. Si te haces seguidor en las redes sociales del Titular el tratamiento de los datos personales se regirá por este apartado, así como por aquellas condiciones de uso, políticas de privacidad y normativas de acceso que pertenezcan a la red social que proceda en cada caso y que has aceptado previamente.</li>
+                            </ul>
+                            <p>El Titular tratará tus datos personales con la finalidad de administrar correctamente su presencia en la red social, informarte de sus actividades, productos o servicios, así como para cualquier otra finalidad que las normativas de las redes sociales permitan.</p>
+                            <p>En ningún caso el Titular utilizará los perfiles de seguidores en redes sociales para enviar publicidad de manera individual.</p>
+                            <h1>Seguridad de los datos personales</h1>
+                            <p>Para proteger tus datos personales, el Titular toma todas las precauciones razonables y sigue las mejores prácticas de la industria para evitar su pérdida, mal uso, acceso indebido, divulgación, alteración o destrucción de los mismos.</p>
+                            <p>El sitio Web está alojado en Digital Ocean. La seguridad de tus datos está garantizada, ya que toman todas las medidas de seguridad necesarias para ello. Puedes consultar su política de privacidad para tener más información.</p>
+                            <h1>Contenido de otros sitios web</h1>
+                            <p>Las páginas de este sitio Web pueden incluir contenido incrustado (por ejemplo, vídeos, imágenes, artículos, etc.). El contenido incrustado de otras web se comporta exactamente de la misma manera que si hubieras visitado la otra web.</p>
+                            <p>Estos sitios Web pueden recopilar datos sobre ti, utilizar cookies, incrustar un código de seguimiento adicional de terceros, y supervisar tu interacción usando este código.</p>
+                            <h1>Política de Cookies</h1>
+                            <p>Para que este sitio Web funcione correctamente necesita utilizar cookies, que es una información que se almacena en tu navegador web.</p>
+                            <p>En la página Política de Cookies puedes consultar toda la información relativa a la política de recogida, la finalidad y el tratamiento de las cookies.</p>
+                            <h1>Legitimación para el tratamiento de datos</h1>
+                            <p>La base legal para el tratamiento de tus datos es: el consentimiento.</p>
+                            <p>Para contactar con el Titular, suscribirte a un boletín o realizar comentarios en este sitio Web tienes que aceptar la presente Política de Privacidad.</p>
+                            <h1>Categorías de datos personales</h1>
+                            <p>Las categorías de datos personales que trata el Titular son:</p>
+                            <ul>
+                                <li>Datos identificativos.</li>
+                            </ul>
+                            <h1>Conservación de datos personales</h1>
+                            <p>Los datos personales que proporciones al Titular se conservarán hasta que solicites su supresión.</p>
+                            <h1>Navegación Web</h1>
+                            <p>Al navegar por Escuelita.dev se pueden recoger datos no identificativos, que pueden incluir, la dirección IP, geolocalización, un registro de cómo se utilizan los servicios y sitios, hábitos de navegación y otros datos que no pueden ser utilizados para identificarte.</p>
+                            <p>El sitio Web utiliza los siguientes servicios de análisis de terceros:</p>
+                            <ul>
+                                <li>Google Analytics</li>
+                                <li>Amplitude.com</li>
+                                <li>Hotjar.com</li>
+                            </ul>
+                            <p>El Titular utiliza la información obtenida para obtener datos estadísticos, analizar tendencias, administrar el sitio, estudiar patrones de navegación y para recopilar información demográfica.</p>
+                            <h1>Exactitud y veracidad de los datos personales</h1>
+                            <p>Te comprometes a que los datos facilitados al Titular sean correctos, completos, exactos y vigentes, así como a mantenerlos debidamente actualizados.</p>
+                            <p>Como Usuario del sitio Web eres el único responsable de la veracidad y corrección de los datos que remitas al sitio exonerando a el Titular de cualquier responsabilidad al respecto.</p>
+                            <h1>Aceptación y consentimiento</h1>
+                            <p>Como Usuario del sitio Web declaras haber sido informado de las condiciones sobre protección de datos de carácter personal, aceptas y consientes el tratamiento de los mismos por parte de el Titular en la forma y para las finalidades indicadas en esta Política de Privacidad.</p>
+                            <h1>Revocabilidad</h1>
+                            <p>Para ejercitar tus derechos de acceso, rectificación, cancelación, portabilidad y oposición tienes que enviar un correo electrónico a hola@escuelita.dev junto con la prueba válida en derecho como una fotocopia del D.N.I. o equivalente.</p>
+                            <p>El ejercicio de tus derechos no incluye ningún dato que el Titular esté obligado a conservar con fines administrativos, legales o de seguridad.</p>
+                            <h1>Cambios en la Política de Privacidad</h1>
+                            <p>El Titular se reserva el derecho a modificar la presente Política de Privacidad para adaptarla a novedades legislativas o jurisprudenciales, así como a prácticas de la industria.</p>
+                            <p>Estas políticas estarán vigentes hasta que sean modificadas por otras debidamente publicadas.</p>
+
+
+                        </div>
+                        <!-- <div class="btn-box"><a href="contact.html" class="theme-btn">Contact us</a></div> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    </div>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $strapi }) {
+    return { 
+        acerca: await $strapi.find('home-acerca-de'),
+    }
+  },    
+}
+</script>
