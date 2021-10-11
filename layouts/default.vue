32a168939b7cf1e08ea6487405e44410c58941fc
Mauricio <mauricio@mauri.co>
2021-10-10 22:00:41 -0300

    Quitar github de cabezal

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,5 @@
 <template>
   <span>
-    <a style="position:absolute; top:0; right:0; z-index:2000" href="https://github.com/Escuelita-dev/sitio-escuelita-dev"><img loading="lazy" width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_right_darkblue_121621.png?resize=149%2C149" class="attachment-full size-full" alt="Fork me on GitHub" data-recalc-dims="1"></a>    
     <!-- .preloader -->
     <!-- <div class="preloader"></div> -->
     <!-- /.preloader -->    
